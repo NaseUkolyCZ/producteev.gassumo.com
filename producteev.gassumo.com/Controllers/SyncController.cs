62836ca580dc8cfce6bc0d72d65e16349acd16c4
David Podhola <david.podhola@naseukoly.cz>
2013-10-17 14:23:58 +0200

    saving token to the DB

--- producteev.gassumo.com/Controllers/SyncController.cs
+++ producteev.gassumo.com/Controllers/SyncController.cs
@@ -1,4 +1,6 @@
-﻿using atollon.sync.nuefdatamodel.Repositories;
+﻿using atollon.sync.communicator.producteev;
+using atollon.sync.idata;
+using atollon.sync.nuefdatamodel.Repositories;
 using Oak.Controllers;
 using PiranhaCMSOak.Support;
 using System;
@@ -13,12 +15,32 @@ namespace producteev.gassumo.com.Controllers
     [MyRequireHttps]
     public class SyncController : PiranhaCMSOak.Controllers.BaseController
     {
+        private ProducteevTasks producteevTasks  = new ProducteevTasks();
+        Accounts accounts = new Accounts();
+
+        public void SetUserToken(int userId, string token)
+        {
+            var account = accounts.SingleWhere("UserId=@0 AND AccountProviderId=@1", args: new object[] { userId, AccountProvider.Producteev });
+            account.Password = token;
+            accounts.Save(account);
+        }
+
         public ActionResult Index()
         {
             var user = User();
             ViewBag.User = user;
+            ViewBag.SetupProducteevToken = producteevTasks.GetAuthenticateUrl();
 
             return View();
+        }
+        public ActionResult Token(string code)
+        {
+            var user = User();
+            ViewBag.User = user;
+            string token = producteevTasks.GetToken(code);
+            SetUserToken(user.Id, token);
+
+            return View("Index");
         }
     }
 }
