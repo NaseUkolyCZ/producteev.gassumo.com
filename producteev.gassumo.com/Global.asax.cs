99617d10a7412c7e0691b37b0ba483e18d87c66b
David Podhola <david.podhola@naseukoly.cz>
2013-10-17 14:37:55 +0200

    MasterSynchronization + log4net

--- producteev.gassumo.com/Global.asax.cs
+++ producteev.gassumo.com/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using atollon.sync.nuefdatamodel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,6 +20,8 @@ namespace producteev.gassumo.com
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            MasterSynchronizer.Start();
         }
     }
 }
