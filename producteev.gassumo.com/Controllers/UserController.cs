5bf7df50f7a7683795ced088b32f1e5b08bb2ab8
David Podhola <david.podhola@naseukoly.cz>
2013-10-17 11:16:58 +0200

    runnable version with Piranha & PiranhaOak

---
+++ producteev.gassumo.com/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace producteev.gassumo.com.Controllers
+{
+    public class UserController : PiranhaCMSOak.Controllers.UserController
+    {
+        protected override Guid GetUserGroupId()
+        {
+            return new Guid("8940b41a-e3a9-44f3-b564-bfd281416142");
+        }
+    }
+}
